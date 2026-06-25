5644dcee4210c5adfe133496f0f3e456be963d05
DHARNEESH B <dharneeshb42@gmail.com>
2026-06-25 22:45:10 +0530

    Time: 3 ms (55.49%), Space: 19.3 MB (70.32%) - LeetHub

--- 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.py
+++ 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.py
@@ -7,13 +7,12 @@ class Solution:
         current_count = 0
         
         for i in range(len(nums)):
-            # If it's the first element or a new distinct number, reset the counter
+           
             if i == 0 or nums[i] != nums[i - 1]:
                 current_count = 1
             else:
                 current_count += 1
                 
-            # Only include the element if it hasn't exceeded the limit 'k'
             if current_count <= k:
                 result.append(nums[i])
                 
