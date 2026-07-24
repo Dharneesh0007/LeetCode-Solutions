21757b5104d6e76bae00bf348c67ee5314ddc51f
DHARNEESH B <dharneeshb42@gmail.com>
2026-07-24 22:39:36 +0530

    Time: 0 ms (100%), Space: 19.9 MB (30.92%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -0,0 +1,8 @@
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        if m * n != r * c:
+            return mat
+        
+        flat = [val for row in mat for val in row]
+        return [flat[i * c:(i + 1) * c] for i in range(r)]
