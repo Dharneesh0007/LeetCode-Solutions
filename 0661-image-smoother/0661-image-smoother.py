ffffd9063e05e437b3e033fde0ec28a82625c659
DHARNEESH B <dharneeshb42@gmail.com>
2026-08-12 20:26:43 +0530

    Time: 172 ms (15.86%), Space: 20.3 MB (10.75%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.py
@@ -0,0 +1,28 @@
+class Solution:
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        m, n = len(img), len(img[0])
+        
+        for r in range(m):
+            for c in range(n):
+                total_sum = 0
+                count = 0
+                
+                for dr in (-1, 0, 1):
+                    for dc in (-1, 0, 1):
+                        nr, nc = r + dr, c + dc
+                        if 0 <= nr < m and 0 <= nc < n:
+                            total_sum += img[nr][nc] & 0xFF
+                            count += 1
+                
+                smoothed_val = total_sum // count
+                img[r][c] |= (smoothed_val << 8)
+        
+        for r in range(m):
+            for c in range(n):
+                img[r][c] >>= 8
+                
+        return img
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
