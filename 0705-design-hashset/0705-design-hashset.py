881456a4333c4e938b6e5d8b9bdc4a0e6b001889
DHARNEESH B <dharneeshb42@gmail.com>
2026-09-04 02:51:26 +0530

    Time: 23 ms (87.1%), Space: 24.5 MB (68.69%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.py
+++ 0705-design-hashset/0705-design-hashset.py
@@ -1,30 +1,25 @@
 class MyHashSet:
 
     def __init__(self):
-        # Collisions koraika oru prime number size select panrom
         self.size = 1009
         self.buckets = [[] for _ in range(self.size)]
 
     def _hash(self, key: int) -> int:
-        # Index calculate panna simple hash function
         return key % self.size
 
     def add(self, key: int) -> None:
         index = self._hash(key)
         bucket = self.buckets[index]
-        # Element bucket-la illana mattum append panrom (duplicates avoid panna)
         if key not in bucket:
             bucket.append(key)
 
     def remove(self, key: int) -> None:
         index = self._hash(key)
         bucket = self.buckets[index]
-        # Element irundha mattum remove panrom
         if key in bucket:
             bucket.remove(key)
 
     def contains(self, key: int) -> bool:
         index = self._hash(key)
         bucket = self.buckets[index]
-        # Element irukka nu check panni boolean value return panrom
         return key in bucket
